29565a4104cdb67ed821f82059b7816cedf06ef7
Mike Ratliff <mratliff@gmail.com>
2019-11-06 11:12:28 -0700

    updated package

--- Package.swift
+++ Package.swift
@@ -3,7 +3,15 @@
 import PackageDescription
 
 let package = Package(
-  name: "SwiftPhoenixClient", 
+  name: "SwiftPhoenixClient",
+  platforms: [
+        .iOS(.v13),
+    ],
+    products: [
+        .library(
+            name: "SwiftPhoenixClient",
+            targets: ["SwiftPhoenixClient"]),
+    ],
   dependencies: [
     .package(url: "https://github.com/daltoniam/Starscream.git", from: "3.0.6"),
   ],
